b025f7a0c722354c55f7a0018cc29ecbe0b5c081
K.Mohammed Siddiq <siddiqmohammed867@gmail.com>
2026-07-27 13:39:14 +0530

    Remove unnecessary outputFileTracingRoot

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,5 @@
-import path from "path";
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  outputFileTracingRoot: path.join(__dirname, ".."),
-};
+const nextConfig: NextConfig = {};
 
 export default nextConfig;
